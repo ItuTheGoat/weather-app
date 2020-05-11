dfae089de0a1729030cdcaebd75f21e14f449ce0
ItuThaGoat <itumelengmochabe@gmail.com>
2020-05-11 18:54:05 +0200

    Completed the app, I might add an Title somwhere

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
+// API Key and base url which I use to call from OpenWeatherMap
 const api = {
   key: "29775669c076456b10bd6dcd389ed448",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
+  // These are the query and result variables that I use to interact with the API
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState("");
 
+  // Search function, uses the above query and result variables
   const search = (e) => {
     if (e.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
@@ -21,6 +24,7 @@ function App() {
     }
   };
 
+  // variables to hold the date information which will be the current system date
   const dateBuilder = (d) => {
     let months = [
       "January",
@@ -54,18 +58,52 @@ function App() {
     return `${day}, ${date} ${month} ${year}`;
   };
 
+  // The function to set the apps background
   function appClassname() {
     let result = "app";
-    if (typeof weather.main != "underfined") {
-      switch ()
+    if (typeof weather.main != "undefined") {
+      switch (weather.weather[0].main) {
+        case "Thunderstorm":
+          result = "app thunder";
+          break;
+        case "Drizzle":
+          result = "app drizzle";
+          break;
+        case "Clouds":
+          result = "app cloudy";
+          break;
+        case "Rain":
+          result = "app rainy";
+          break;
+        case "Clear":
+          result = "app clear";
+          break;
+        case "Tornado":
+          result = "app tornado";
+          break;
+        case "Mist":
+          result = "app misty";
+          break;
+        case "Fog":
+          result = "app misty";
+          break;
+        case "Tornado":
+          result = "app tornado";
+          break;
+        case "Snowy":
+          result = "app snowy";
+          break;
+        default:
+          result = "app";
+      }
     } else {
       result = "app";
     }
-
     return result;
   }
 
   return (
+    // This div calls the function that sets the background
     <div className={appClassname()}>
       <main>
         <div className="search-box">
@@ -101,13 +139,3 @@ function App() {
 }
 
 export default App;
-
-// <div
-//       className={
-//         typeof weather.main != "undefined"
-//           ? weather.main.temp > 16
-//             ? "app warm"
-//             : "app"
-//           : "app"
-//       }
-//     ></div>
